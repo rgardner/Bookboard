7729585038d24e8ab105216991e7d65427b4543b
Robert Gardner <rhg259@nyu.edu>
2013-07-27 14:55:58 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookboard::Application.config.secret_key_base = 'b6a1cbfb288d9dd8badd77a1358ac2722197405b09cd13877bc08333e0794f58e279819a48e39e15f42edb2bfe8af855b7a20f1cb9e1f0f9706c7a2d089576d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bookboard::Application.config.session_store :cookie_store, key: '_bookboard_session'
