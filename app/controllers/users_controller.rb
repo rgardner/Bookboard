cb931d5654869ab01371420d98acf7969d92e3cc
Robert Gardner <rhg259@nyu.edu>
2013-07-29 00:59:36 -0700

    Add Initial profile page with styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,15 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,11 +9,21 @@ describe "User Pages" do
     it { should have_selector('h1', text: heading) }      
   end
 
-  describe "signup page" do
+  describe "Signup page" do
     before { visit signup_path }
     let(:page_title) { 'Sign up' }
     let(:heading)    { 'Sign up' }
 
     it_should_behave_like "all user pages"
+  end
+
+  describe "Profile page" do
+    before { visit user_path(user) }
+    let(:page_title) { user.name }
+    let(:heading)    { user.name }
+    let(:user) { FactoryGirl.create(:user) }
+
+    it_should_behave_like "all user pages"
+
   end
 end
