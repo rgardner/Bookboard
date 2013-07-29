a7c49d6bfd8c51dc7aa574c6d32e771298b3c5ef
Robert Gardner <rhg259@nyu.edu>
2013-07-28 22:59:17 -0700

    Add signup examples, format, and use have_selector

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -6,11 +6,17 @@ describe "UserPages" do
     
     subject { page }
 
+    shared_examples_for "all user pages" do
+      it { should have_title(full_title(page_title)) }
+      it { should have_selector('h1', text: heading) }      
+    end
+
     describe "signup page" do
       before { visit signup_path }
+      let(:page_title) { 'Sign up' }
+      let(:heading)    { 'Sign up' }
 
-      it { should have_content('Sign up') }
-      it { should have_title(full_title('Sign up')) }
+      it_should_behave_like "all user pages"
     end
   end
 end
