06d7aa6f458268d7d67f7dfb15d75cb101230c8a
Robert Gardner <rhg259@nyu.edu>
2013-07-29 01:45:11 -0700

    Implement initial user signup and form styling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    # redirect_to root_path if signed_in?
+    @user = User.new(user_params)
+    if @user.save
+      # sign_in @user
+      flash[:success] = "Welcome to Bookboard!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 
   def show
