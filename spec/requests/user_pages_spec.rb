6dd477a908ce332d2cf6afa48074fc889436ff16
Robert Gardner <rhg259@nyu.edu>
2013-07-28 23:39:12 -0700

    Remove extraneous user pages description

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,22 +1,19 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User Pages" do
   
-  describe "User pages" do
-    
-    subject { page }
+  subject { page }
 
-    shared_examples_for "all user pages" do
-      it { should have_title(full_title(page_title)) }
-      it { should have_selector('h1', text: heading) }      
-    end
+  shared_examples_for "all user pages" do
+    it { should have_title(full_title(page_title)) }
+    it { should have_selector('h1', text: heading) }      
+  end
 
-    describe "signup page" do
-      before { visit signup_path }
-      let(:page_title) { 'Sign up' }
-      let(:heading)    { 'Sign up' }
+  describe "signup page" do
+    before { visit signup_path }
+    let(:page_title) { 'Sign up' }
+    let(:heading)    { 'Sign up' }
 
-      it_should_behave_like "all user pages"
-    end
+    it_should_behave_like "all user pages"
   end
 end
